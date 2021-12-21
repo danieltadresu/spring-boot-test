092e7b01e251cbab546dfdddc5a0fb5bf07dbe01
danieltadresu <danieldev1999@gmail.com>
2021-12-20 21:20:33 -0300

    :sparkles: add new entry point GreetingController

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -1,21 +1,35 @@
-package com.example.demo;
+// package com.example.demo;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+// import java.util.concurrent.atomic.AtomicLong;
 
-@SpringBootApplication
-@RestController
-public class DemoApplication {
+// import com.example.restservice.Greeting;
 
-  public static void main(String[] args) {
-    SpringApplication.run(DemoApplication.class, args);
-  }
+// import org.springframework.boot.SpringApplication;
+// import org.springframework.boot.autoconfigure.SpringBootApplication;
+// import org.springframework.web.bind.annotation.GetMapping;
+// import org.springframework.web.bind.annotation.RequestParam;
+// import org.springframework.web.bind.annotation.RestController;
 
-  @GetMapping("/hello")
-  public String hello(@RequestParam(value = "name", defaultValue = "World") String name) {
-    return String.format("Hello %s!", name);
-  }
-}
+// @SpringBootApplication
+// @RestController
+// public class DemoApplication {
+
+//   public static void main(String[] args) {
+//     SpringApplication.run(DemoApplication.class, args);
+//   }
+
+//   private static final String template = "Hello, %s!";
+//   private final AtomicLong counter = new AtomicLong();
+
+//   @GetMapping("/hello")
+//   public String hello(@RequestParam(value = "name", defaultValue = "World") String name) {
+//     return String.format("Hello %s!", name);
+//   }
+
+//   @GetMapping("/greeting")
+// 	public Greeting greeting(@RequestParam(value = "name", defaultValue = "World") String name) {
+//     System.out.println("Hello world!");
+// 		return new Greeting(counter.incrementAndGet(), String.format(template, name));
+// 	}
+// }
+
